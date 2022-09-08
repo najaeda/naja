9ca2095659ad6eb42c78cac6d956ea35429e1344
xtof <christophe.alex@gmail.com>
2022-09-08 14:49:44 +0200

    Core headers installation

--- test/core/NajaPrivatePropertyTest.cpp
+++ test/core/NajaPrivatePropertyTest.cpp
@@ -39,10 +39,32 @@ class TestObject: public NajaObject {
 
 class TestPrivateProperty: public NajaPrivateProperty {
   public:
-    NajaPrivateProperty* create(const std::string& name) {
-      return nullptr;
+    using super = NajaPrivateProperty;
+    static const inline std::string Name = "TestPrivateProperty";
+    static void preCreate(NajaObject* object) {
+      super::preCreate(object, Name);
+    }
+
+    void postCreate(NajaObject* owner) {
+      super::postCreate(owner);
+    }
+
+    static TestPrivateProperty* create(NajaObject* owner) {
+      preCreate(owner);
+      TestPrivateProperty* property = new TestPrivateProperty();
+      property->postCreate(owner);
+      return property;
+    }
+
+    std::string getName() const override {
+      return Name;
+    }
+
+    std::string getString() const override {
+      return getName();
     }
 };
+
 
 }
 
@@ -63,5 +85,10 @@ class NajaPrivatePropertyTest: public ::testing::Test {
 };
 
 TEST_F(NajaPrivatePropertyTest, test) {
-
+  ASSERT_NE(nullptr, testObject_);
+  TestPrivateProperty* property = TestPrivateProperty::create(testObject_);
+  ASSERT_NE(nullptr, property);
+  EXPECT_TRUE(testObject_->hasProperty(TestPrivateProperty::Name));
+  EXPECT_EQ(property, testObject_->getProperty(TestPrivateProperty::Name));
+  EXPECT_EQ(1, testObject_->getProperties().size());
 }
