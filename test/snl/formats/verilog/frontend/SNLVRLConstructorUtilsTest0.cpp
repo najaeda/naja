1888f2dc159d61bd47fcbd79b088c587d27eefc0
xtof <christophe.alex@gmail.com>
2023-03-23 16:25:37 +0100

    More constant testing

--- test/snl/formats/verilog/frontend/SNLVRLConstructorUtilsTest0.cpp
+++ test/snl/formats/verilog/frontend/SNLVRLConstructorUtilsTest0.cpp
@@ -30,22 +30,25 @@ TEST(SNLVRLConstructorUtilsTest0, testDecimal) {
 }
 
 TEST(SNLVRLConstructorUtilsTest0, testHexadecimal) {
-  auto num = naja::verilog::BasedNumber("4", "h", "2");
+  for (size_t i=0; i<15; i++) {
+    std::stringstream stream;
+    stream << std::hex << i;
+    std::string hexStr = stream.str();
+    auto num = naja::verilog::BasedNumber("4", "h", hexStr);
+    auto bits = SNLVRLConstructorUtils::numberToBits(num);
+    boost::dynamic_bitset<> ref(4, 0ul); 
+    int intNum = i;
+    int j = 0;
+    while (intNum > 0) {
+      ref[j] = intNum % 2;
+		  j++;
+		  intNum = intNum / 2;
+    }
+    EXPECT_EQ(ref, bits);
+  }
+
+  auto num = naja::verilog::BasedNumber("2", "h", "D");
   auto bits = SNLVRLConstructorUtils::numberToBits(num);
-  EXPECT_FALSE(bits[3]);
-  EXPECT_FALSE(bits[2]);
-  EXPECT_TRUE(bits[1]);
-  EXPECT_FALSE(bits[0]);
-
-  num = naja::verilog::BasedNumber("4", "h", "9");
-  bits = SNLVRLConstructorUtils::numberToBits(num);
-  EXPECT_TRUE(bits[3]);
-  EXPECT_FALSE(bits[2]);
-  EXPECT_FALSE(bits[1]);
-  EXPECT_TRUE(bits[0]);
-
-  num = naja::verilog::BasedNumber("2", "h", "D");
-  bits = SNLVRLConstructorUtils::numberToBits(num);
   EXPECT_FALSE(bits[3]); //truncated 
   EXPECT_FALSE(bits[2]); //truncated
   EXPECT_FALSE(bits[1]);
