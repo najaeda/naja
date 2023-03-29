09a4b6abf0ffac757083b292bad3a27231673f45
xtof <christophe.alex@gmail.com>
2023-03-29 17:51:01 +0200

    start parsing assigns

--- test/snl/formats/verilog/benchmarks/test1.v
+++ test/snl/formats/verilog/benchmarks/test1.v
@@ -11,7 +11,12 @@ module model(i, o, io);
 endmodule
 
 module test(input i, output o, inout io);
-  wire net0;
+  wire n0;
+  wire n1;
+  wire n2;
+  wire n3;
 
-  model inst();
+  model inst(.i(n0), .o(n1), .io(n2));
+  assign n0 = n3;
+  assign n3 = 1'b0; 
 endmodule
