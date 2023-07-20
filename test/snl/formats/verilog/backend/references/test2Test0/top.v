c072ce6ec4823d3a2a4749fc5ef30f69417da7d8
xtof <christophe.alex@gmail.com>
2023-07-20 20:46:59 +0200

    update references

--- test/snl/formats/verilog/backend/references/test2Test0/top.v
+++ test/snl/formats/verilog/backend/references/test2Test0/top.v
@@ -1,9 +1,3 @@
-// Copyright The Naja Authors.
-// SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/xtofalex/naja/blob/main/AUTHORS>
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-
 module top();
 wire [-2:2] bus;
 wire scalar;
--- test/snl/formats/verilog/backend/references/testParameters0/top.v
+++ test/snl/formats/verilog/backend/references/testParameters0/top.v
@@ -1,9 +1,3 @@
-// Copyright The Naja Authors.
-// SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/xtofalex/naja/blob/main/AUTHORS>
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-
 module model();
 parameter PARAM0 = 8 ;
 parameter PARAM1 = "FALSE" ;
