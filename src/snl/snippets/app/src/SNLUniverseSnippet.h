99ee08afeeeb4bb39d2d6aafc719f02338f3684e
xtof <christophe.alex@gmail.com>
2023-10-31 11:37:47 +0100

    License cleaning

--- src/snl/snippets/app/src/SNLUniverseSnippet.h
+++ src/snl/snippets/app/src/SNLUniverseSnippet.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/xtofalex/naja/blob/main/AUTHORS>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef __SNL_UNIVERSE_SNIPPET_H_
 #define __SNL_UNIVERSE_SNIPPET_H_
 
