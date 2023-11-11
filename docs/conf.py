a17db0aba2b303ed96cf98f5e0febda5e0f8dd2d
xtof <christophe.alex@gmail.com>
2023-11-11 19:56:32 +0100

    breathe config

--- docs/conf.py
+++ docs/conf.py
@@ -14,3 +14,6 @@ import subprocess
 subprocess.call('mkdir build', shell=True)
 subprocess.call('cd build; cmake ../.. -DBUILD_ONLY_DOC=ON', shell=True)
 subprocess.call('cd build; make docs', shell=True)
+
+breathe_projects = { "naja" : "./build/xml/" }
+breathe_default_project = "naja"
