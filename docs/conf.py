17b9212bac7971e27fb6dbd20d1dd68026cd320f
xtof <christophe.alex@gmail.com>
2023-11-11 10:21:32 +0100

    run cmake

--- docs/conf.py
+++ docs/conf.py
@@ -10,4 +10,7 @@ html_theme = "sphinx_rtd_theme"
 breathe_default_project = "naja"
 
 import subprocess
-subprocess.call('doxygen', shell=True)
+#call doxygen from cmake
+subprocess.call('mkdir build', shell=True)
+subprocess.call('cd build; cmake ..', shell=True)
+subprocess.call('cd build; make docs', shell=True)
