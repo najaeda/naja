f245c8252affddd268e898007f2138814fa9abdb
Noam Cohen <noam.chn1@gmail.com>
2024-02-16 16:31:29 +0100

    fixes

--- test/dnl/DNLTests.cpp
+++ test/dnl/DNLTests.cpp
@@ -56,8 +56,8 @@ TEST_F(DNLTests, SNLDataAccess) {
     assert(dnl->getTop().getSNLModel() != nullptr);
     //Validate the access to the SNL data
     DNLID modID = dnl->getTop().getID();
-    DNLID inTermID = dnl->getTop().getTerminal(inTerm).getID();
-    DNLID outTermID = dnl->getTop().getTerminal(outTerm).getID();;
+    DNLID inTermID = dnl->getTop().getTerminalFromBitTerm(inTerm).getID();
+    DNLID outTermID = dnl->getTop().getTerminalFromBitTerm(outTerm).getID();;
     EXPECT_EQ(modID, 0);
     EXPECT_EQ(inTermID, 0);
     EXPECT_EQ(outTermID, 1);
@@ -67,12 +67,94 @@ TEST_F(DNLTests, SNLDataAccess) {
     SNLUniverse::get()->destroy();
 }
 
+TEST_F(DNLTests, SNLDataAccessWith2levelsOfHierarchy) {
+    //Create one module snl with one input and one output
+    SNLUniverse* univ = SNLUniverse::create();
+    SNLDB* db = SNLDB::create(univ);
+    SNLLibrary* library = SNLLibrary::create(db, SNLName("MYLIB"));
+    SNLDesign* mod = SNLDesign::create(library, SNLName("mod"));
+    univ->setTopDesign(mod);
+    auto inTerm = SNLScalarTerm::create(mod, SNLTerm::Direction::Input, SNLName("in"));
+    auto outTerm = SNLScalarTerm::create(mod, SNLTerm::Direction::Output, SNLName("out"));
+    //Create a sub module snl with one input and one output
+    SNLDesign* submod = SNLDesign::create(library, SNLName("submod"));
+    auto subinTerm = SNLScalarTerm::create(submod, SNLTerm::Direction::Input, SNLName("subin"));
+    auto suboutTerm = SNLScalarTerm::create(submod, SNLTerm::Direction::Output, SNLName("subout"));
+    SNLInstance* subinst = SNLInstance::create(mod, submod, SNLName("subinst"));
+    //Create a DNL on top of the SNL
+    DNL* dnl = DNL::create();
+    assert(dnl != nullptr);
+    assert(dnl->getTop().getSNLModel() != nullptr);
+    //Validate the access to the SNL data
+    DNLID modID = dnl->getTop().getID();
+    DNLID inTermID = dnl->getTop().getTerminalFromBitTerm(inTerm).getID();
+    DNLID outTermID = dnl->getTop().getTerminalFromBitTerm(outTerm).getID();;
+    DNLID submodID = dnl->getTop().getChildInstance(subinst).getID();
+    DNLID subinTermID = dnl->getTop().getChildInstance(subinst).getTerminalFromBitTerm(subinTerm).getID();
+    DNLID suboutTermID = dnl->getTop().getChildInstance(subinst).getTerminalFromBitTerm(suboutTerm).getID();;
+    EXPECT_EQ(modID, 0);
+    EXPECT_EQ(inTermID, 0);
+    EXPECT_EQ(outTermID, 1);
+    EXPECT_EQ(submodID, 1);
+    EXPECT_EQ(subinTermID, 2);
+    EXPECT_EQ(suboutTermID, 3);
+    //Destroy the DNL
+    DNL::destroy();
+    //Destroy the SNL
+    SNLUniverse::get()->destroy();
+}
+
+TEST_F(DNLTests, SNLDataAccessWith3levelsOfHierarchy) {
+    //Create one module snl with one input and one output
+    SNLUniverse* univ = SNLUniverse::create();
+    SNLDB* db = SNLDB::create(univ);
+    SNLLibrary* library = SNLLibrary::create(db, SNLName("MYLIB"));
+    SNLDesign* mod = SNLDesign::create(library, SNLName("mod"));
+    univ->setTopDesign(mod);
+    auto inTerm = SNLScalarTerm::create(mod, SNLTerm::Direction::Input, SNLName("in"));
+    auto outTerm = SNLScalarTerm::create(mod, SNLTerm::Direction::Output, SNLName("out"));
+    //Create a DNL on top of the SNL
+    
+   
+    //Create a sub module snl with one input and one output
+    SNLDesign* submod = SNLDesign::create(library, SNLName("submod"));
+    auto subinTerm = SNLScalarTerm::create(submod, SNLTerm::Direction::Input, SNLName("subin"));
+    auto suboutTerm = SNLScalarTerm::create(submod, SNLTerm::Direction::Output, SNLName("subout"));
+    SNLInstance* subinst = SNLInstance::create(mod, submod, SNLName("subinst"));
+    //Create a DNL on top of the SNL
     
     
-
-
-
-
-
+    //Create a sub module snl with one input and one output
+    SNLDesign* subsubmod = SNLDesign::create(library, SNLName("subsubmod"));
+    auto subsubinTerm = SNLScalarTerm::create(subsubmod, SNLTerm::Direction::Input, SNLName("subsubin"));
+    auto subsuboutTerm = SNLScalarTerm::create(subsubmod, SNLTerm::Direction::Output, SNLName("subsubout"));
+    SNLInstance* subsubinst = SNLInstance::create(submod, subsubmod, SNLName("subsubinst"));
+    //Create a DNL on top of the SNL
+    //Validate the access to the SNL data
+    DNL* dnl = DNL::create();
+     assert(dnl->getTop().getSNLModel() != nullptr);
+    DNLID modID = dnl->getTop().getID();
+    DNLID inTermID = dnl->getTop().getTerminalFromBitTerm(inTerm).getID();
+    DNLID outTermID = dnl->getTop().getTerminalFromBitTerm(outTerm).getID();;
+    DNLID submodID = dnl->getTop().getChildInstance(subinst).getID();
+    DNLID subinTermID = dnl->getTop().getChildInstance(subinst).getTerminalFromBitTerm(subinTerm).getID();
+    DNLID suboutTermID = dnl->getTop().getChildInstance(subinst).getTerminalFromBitTerm(suboutTerm).getID();;
+    DNLID subsubmodID = dnl->getTop().getChildInstance(subinst).getChildInstance(subsubinst).getID();
+    DNLID subsubinTermID = dnl->getTop().getChildInstance(subinst).getChildInstance(subsubinst).getTerminalFromBitTerm(subsubinTerm).getID();
+    DNLID subsuboutTermID = dnl->getTop().getChildInstance(subinst).getChildInstance(subsubinst).getTerminalFromBitTerm(subsuboutTerm).getID();;
+    EXPECT_EQ(modID, 0);
+    EXPECT_EQ(inTermID, 0);
+    EXPECT_EQ(outTermID, 1);
+    EXPECT_EQ(submodID, 1);
+    EXPECT_EQ(subinTermID, 2);
+    EXPECT_EQ(suboutTermID, 3);
+    EXPECT_EQ(subsubmodID, 2);
+    EXPECT_EQ(subsubinTermID, 4);
+    EXPECT_EQ(subsuboutTermID, 5);
+    //Destroy the DNL
+    DNL::destroy();
+    //Destroy the SNL
+    SNLUniverse::get()->destroy();
+}
 
 
