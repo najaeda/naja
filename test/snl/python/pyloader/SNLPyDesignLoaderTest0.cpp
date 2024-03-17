a2c1b74b66825a643d1ad0708bb2c11723fcd9a1
xtof <christophe.alex@gmail.com>
2024-03-17 19:28:55 +0100

    more testing

---
+++ test/snl/python/pyloader/SNLPyDesignLoaderTest0.cpp
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "gtest/gtest.h"
+
+#include "SNLUniverse.h"
+#include "SNLScalarTerm.h"
+#include "SNLPyLoader.h"
+#include "SNLException.h"
+using namespace naja::SNL;
+
+#ifndef SNL_PRIMITIVES_TEST_PATH
+#define SNL_PRIMITIVES_TEST_PATH "Undefined"
+#endif
+
+class SNLPyDesignLoaderTest0: public ::testing::Test {
+  protected:
+    void SetUp() override {
+      auto universe = SNLUniverse::create();
+      auto db = SNLDB::create(universe);
+      auto primitives = SNLLibrary::create(db, SNLLibrary::Type::Primitives, SNLName("primitives"));
+      auto primitive = SNLDesign::create(primitives, SNLDesign::Type::Primitive, SNLName("primitive"));
+      auto pi = SNLScalarTerm::create(primitive, SNLTerm::Direction::Input, SNLName("i"));
+      auto po = SNLScalarTerm::create(primitive, SNLTerm::Direction::Output, SNLName("o"));
+      auto designsLibrary = SNLLibrary::create(db, SNLName("designs"));
+      design_ = SNLDesign::create(designsLibrary, SNLName("design"));
+    }
+    void TearDown() override {
+      if (SNLUniverse::get()) {
+        SNLUniverse::get()->destroy();
+      }
+    }
+    SNLDesign* design_;
+};
+
+TEST_F(SNLPyDesignLoaderTest0, test) {
+  auto scriptPath = std::filesystem::path(SNL_PRIMITIVES_TEST_PATH);
+  scriptPath /= "scripts";
+  scriptPath /= "design_loader.py";
+  SNLPyLoader::loadDesign(design_, scriptPath);
+
+  //ASSERT_EQ(2, db->getLibraries().size());
+}
+
+TEST_F(SNLPyDesignLoaderTest0, testDesignLoadingError) {
+  auto scriptPath = std::filesystem::path(SNL_PRIMITIVES_TEST_PATH);
+  scriptPath /= "scripts";
+  scriptPath /= "design_faulty.py";
+  EXPECT_THROW(SNLPyLoader::loadDesign(design_, scriptPath), SNLException);
+}
