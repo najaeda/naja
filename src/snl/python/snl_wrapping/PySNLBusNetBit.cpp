87b97f300c2bb6abeb4286dfcaf6a9772e515e76
Christophe Alexandre <christophe.alex@gmail.com>
2024-04-04 14:10:16 +0200

    Fix and testing (#60)
    * Fix and testing
    
    * coverage
    
    * refine python dump verilog
    
    * error testing

--- src/snl/python/snl_wrapping/PySNLBusNetBit.cpp
+++ src/snl/python/snl_wrapping/PySNLBusNetBit.cpp
@@ -6,8 +6,7 @@
 
 #include "PyInterface.h"
 #include "PySNLDesign.h"
-
-#include "SNLBusNetBit.h"
+#include "PySNLBusNet.h"
 
 namespace PYSNL {
 
@@ -16,10 +15,17 @@ using namespace naja::SNL;
 #undef   ACCESS_OBJECT
 #undef   ACCESS_CLASS
 #define  ACCESS_OBJECT           parent_.parent_.parent_.object_
-#define  ACCESS_CLASS(_pyObject)  &(_pyObject->parent_)
-#define  METHOD_HEAD(function)   GENERIC_METHOD_HEAD(BusNetBit, net, function)
+#define  ACCESS_CLASS(_pyObject) &(_pyObject->parent_)
+#define  METHOD_HEAD(function) GENERIC_METHOD_HEAD(SNLBusNetBit, function)
+
+DirectGetIntMethod(PySNLBusNetBit_getBit, getBit, PySNLBusNetBit, SNLBusNetBit)
+GetObjectMethod(BusNetBit, BusNet, getBus)
 
 PyMethodDef PySNLBusNetBit_Methods[] = {
+  { "getBit", (PyCFunction)PySNLBusNetBit_getBit, METH_NOARGS,
+    "get SNLBusNetBit Bit value"},
+  { "getBus", (PyCFunction)PySNLBusNetBit_getBus, METH_NOARGS,
+    "get SNLBusNetBit Bus net"},
   {NULL, NULL, 0, NULL}           /* sentinel */
 };
 
