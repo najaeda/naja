9a8bcf629d5d46b7684e704790363367879af443
xtof <christophe.alex@gmail.com>
2024-04-18 14:57:10 +0200

    adding license

--- src/python/naja/stats/design_stats.py
+++ src/python/naja/stats/design_stats.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import logging
 from naja import snl
 
