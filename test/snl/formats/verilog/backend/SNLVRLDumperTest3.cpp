6a5b641823e9282c7bbe1391feadcc0a0bc2eb01
xtof <christophe.alex@gmail.com>
2024-04-23 12:56:32 +0200

    new test

---
+++ test/snl/formats/verilog/backend/SNLVRLDumperTest3.cpp
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "gtest/gtest.h"
+
+#include <filesystem>
+#include <fstream>
+
+#include "SNLVRLDumper.h"
+
+#include "SNLUniverse.h"
+#include "SNLDB.h"
+#include "SNLScalarTerm.h"
+#include "SNLBusTerm.h"
+
+using namespace naja::SNL;
+
+#ifndef SNL_VRL_DUMPER_TEST_PATH
+#define SNL_VRL_DUMPER_TEST_PATH "Undefined"
+#endif
+#ifndef SNL_VRL_DUMPER_REFERENCES_PATH
+#define SNL_VRL_DUMPER_REFERENCES_PATH "Undefined"
+#endif
+
+//Test large interface designs
+class SNLVRLDumperTest3: public ::testing::Test {
+  protected:
+    void SetUp() override {
+      SNLUniverse* universe = SNLUniverse::create();
+      db_ = SNLDB::create(universe);
+      SNLLibrary* library = SNLLibrary::create(db_, SNLName("MYLIB"));
+      SNLDesign* design = SNLDesign::create(library, SNLName("top"));
+
+      SNLScalarTerm::create(design, SNLTerm::Direction::Input, SNLName("i0"));
+      SNLBusTerm::create(design, SNLTerm::Direction::Input, 31, 0, SNLName("i1"));
+      SNLScalarTerm::create(design, SNLTerm::Direction::Output, SNLName("o"));
+      SNLBusTerm::create(design, SNLTerm::Direction::Input, 31, 0, SNLName("i2"));
+      for (int i = 0; i < 200; i++) {
+        SNLScalarTerm::create(design, SNLTerm::Direction::Output, SNLName("o_" + std::to_string(i)));
+      }
+    }
+    void TearDown() override {
+      SNLUniverse::get()->destroy();
+    }
+  protected:
+    SNLDB*      db_;
+};
+
+TEST_F(SNLVRLDumperTest3, test) {
+  auto lib = db_->getLibrary(SNLName("MYLIB"));  
+  ASSERT_TRUE(lib);
+  auto top = lib->getDesign(SNLName("top"));
+  ASSERT_TRUE(top);
+  std::filesystem::path outPath(SNL_VRL_DUMPER_TEST_PATH);
+  outPath = outPath / "test3";
+  if (std::filesystem::exists(outPath)) {
+    std::filesystem::remove_all(outPath);
+  }
+  std::filesystem::create_directory(outPath);
+  SNLVRLDumper dumper;
+  dumper.setTopFileName(top->getName().getString() + ".v");
+  dumper.setSingleFile(true);
+  dumper.dumpDesign(top, outPath);
+
+  std::filesystem::path referencePath(SNL_VRL_DUMPER_REFERENCES_PATH);
+  referencePath = referencePath / "test3" / "top.v";
+  ASSERT_TRUE(std::filesystem::exists(referencePath));
+  std::string command = "diff " + outPath.string() + " " + referencePath.string();
+  EXPECT_FALSE(std::system(command.c_str()));
+}
