f86423f0128e248703299d4a61d4f13882f952b1
Noam Cohen <noam.chn1@gmail.com>
2024-11-13 21:50:02 +0100

    separate load primitives and design

--- test/snl/python/snl_wrapping/test_snldb.py
+++ test/snl/python/snl_wrapping/test_snldb.py
@@ -26,10 +26,12 @@ class SNLDBTest(unittest.TestCase):
   
   def testVerilog(self):
     u = snl.SNLUniverse.get()
+    db = snl.SNLDB.create(u) 
     self.assertIsNotNone(u)
     designs = ["../../../../../test/snl/formats/verilog/benchmarks/test0.v"]
     primitives = ["../../../../../test/snl/formats/liberty/benchmarks/asap7_excerpt/test0.lib"]
-    db = snl.SNLDB.loadVerilog(primitives, designs)
+    db.loadLibertyPrimitives(primitives)
+    db.loadVerilog(designs)
     db.dumpVerilog("./test_verilog")
     with self.assertRaises(SystemError) as context: db.dumpVerilog()
     with self.assertRaises(SystemError) as context: db.dumpVerilog(-1)
@@ -56,22 +58,23 @@ class SNLDBTest(unittest.TestCase):
     with self.assertRaises(RuntimeError) as context: snl.SNLDB.create("ERROR")
     with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadSNL(u)
     with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadSNL("./test_verilogError.v")
-    u.destroy()
     primitives = [1]
     designs = [2]
     primitivesNoExtention = ["../../../../../test/snl/formats/liberty/benchmarks/asap7_excerpt/test0"]
     primitivesCorrect = ["../../../../../test/snl/formats/liberty/benchmarks/asap7_excerpt/test0.lib"]
-    primitivesWrongExtention = ["../../../../../test/snl/formats/liberty/benchmarks/asap7_excerpt/test0.lib"]
+    primitivesWrongExtention = ["../../../../../test/snl/formats/liberty/benchmarks/asap7_excerpt/test0.sd"]
+    with self.assertRaises(SystemError) as context: db.loadVerilog("Error", "Error")
+    with self.assertRaises(SystemError) as context: db.loadLibertyPrimitives("Error", "Error")
+    with self.assertRaises(SystemError) as context: db.loadVerilog("Error")
+    with self.assertRaises(SystemError) as context: db.loadLibertyPrimitives("Error")
+    with self.assertRaises(SystemError) as context: db.loadLibertyPrimitives(primitives)
+    with self.assertRaises(SystemError) as context: db.loadVerilog(designs)
+    with self.assertRaises(SystemError) as context: db.loadLibertyPrimitives(primitivesNoExtention)
+    with self.assertRaises(SystemError) as context: db.loadLibertyPrimitives(primitivesWrongExtention)
+    u.destroy()
     with self.assertRaises(RuntimeError) as context: snl.SNLDB.create(u)
     with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadSNL()
     with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadSNL("./error")
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog("Error", "Error", "Error")
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog(primitivesCorrect, "Error")
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog("Error", "Error")
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog(primitives, designs)
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog(primitivesCorrect, designs)
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog(primitivesNoExtention, designs)
-    with self.assertRaises(RuntimeError) as context: snl.SNLDB.loadVerilog(primitivesWrongExtention, designs)
     
 if __name__ == '__main__':
   faulthandler.enable()
