53c49dd14fd9f111dcf94c006f69208077744174
xtof <christophe.alex@gmail.com>
2024-11-19 15:04:16 +0100

    WIP

--- setup.py
+++ setup.py
@@ -2,18 +2,33 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import platform
 from skbuild import setup
+
+library_names = []
+
+# Determine library based on OS
+if platform.system() == "Darwin":
+    libraries = [f"lib{x}.dylib" for x in library_names]
+else:  # Assume Linux
+    libraries = [f"lib{x}.so" for x in library_names]
+
+libraries.append("install/lib/python/naja/snl.so")
+#libraries.append("snl.so")
 
 setup(
     name="naja",
     version="0.1.0",
-    description="A Python package with a C++ backend built using CMake",
-    author="Your Name",
-    author_email="your.email@example.com",
-    license="MIT",
+    description="Naja Python package",
+    author="Naja Authors",
+    author_email="contact@keplertech.io",
+    license="Apache License 2.0",
     packages=["naja"],
-    cmake_install_dir="lib/python/naja",
+    package_dir={"naja": "src/naja_python"},
+    cmake_install_dir="install",
+    #cmake_install_dir="src/naja_python",
     include_package_data=True,
+    package_data={"naja": libraries},
     cmake_args=[
         "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",      # Enable verbose output
         "-DCMAKE_BUILD_TYPE=Release",            # Set the build type
