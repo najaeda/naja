1cc6c11d8879d685e2c48a40ee64a3393cbb3f5e
xtof <christophe.alex@gmail.com>
2024-11-20 09:34:31 +0100

    WIP

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import platform
+#import platform
 from skbuild import setup
 
-library_names = []
+#library_names = []
 
 # Determine library based on OS
-if platform.system() == "Darwin":
-    libraries = [f"lib{x}.dylib" for x in library_names]
-else:  # Assume Linux
-    libraries = [f"lib{x}.so" for x in library_names]
-
-libraries.append("install/lib/python/naja/snl.so")
+#if platform.system() == "Darwin":
+#    libraries = [f"lib{x}.dylib" for x in library_names]
+#else:  # Assume Linux
+#    libraries = [f"lib{x}.so" for x in library_names]
+#
+#libraries.append("install/lib/python/naja/snl.so")
 #libraries.append("snl.so")
 
 setup(
@@ -24,11 +24,11 @@ setup(
     author_email="contact@keplertech.io",
     license="Apache License 2.0",
     packages=["naja"],
-    package_dir={"naja": "src/naja_python"},
-    cmake_install_dir="install",
+    #package_dir={"naja": "src/naja_python"},
+    #cmake_install_dir="install",
     #cmake_install_dir="src/naja_python",
-    include_package_data=True,
-    package_data={"naja": libraries},
+    #include_package_data=True,
+    #package_data={"naja": libraries},
     cmake_args=[
         "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",      # Enable verbose output
         "-DCMAKE_BUILD_TYPE=Release",            # Set the build type
