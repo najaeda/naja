b3148e690cd85eb6f4290c87d53e38c362784931
xtof <christophe.alex@gmail.com>
2024-12-17 16:29:13 +0100

    update example

--- najaeda_examples/count_leaves/najaeda_count_leaves.py
+++ najaeda_examples/count_leaves/najaeda_count_leaves.py
@@ -16,13 +16,21 @@ benchmarks = path.join('..','benchmarks')
 top = netlist.load_verilog([path.join(benchmarks, 'verilog', 'arm_core_netlist.v')])
 
 # snippet-start: count_leaves
-leaves = {"count": 0}
+leaves = {"count": 0, "assigns": 0, "constants": 0}
 def count_leaves(instance, leaves):
     if instance.is_leaf():
-        leaves["count"] += 1
+        if instance.is_assign():
+            leaves["assigns"] += 1
+        elif instance.is_const():
+            leaves["constants"] += 1
+        else:
+            leaves["count"] += 1
 visitor_config = instance_visitor.VisitorConfig(callback=count_leaves, args=(leaves,))
 instance_visitor.Visitor(top).visit(top, visitor_config)
-print(f"{top} nb leaves={leaves['count']}")
+print(f"{top} leaves count")
+print(f"nb_assigns={leaves['assigns']}")
+print(f"nb constants={leaves['constants']}")
+print(f"nb other leaves={leaves['count']}")
 # snippet-end: count_leaves
 
 sys.exit(0)
