c7e3554b80772c109890b5ccfe4a587dd0d487e0
Noam Cohen <noam.chn1@gmail.com>
2025-01-03 23:51:08 +0100

    coverage

--- test/najaeda/test_najaeda_netlist_xilinx.py
+++ test/najaeda/test_najaeda_netlist_xilinx.py
@@ -35,20 +35,30 @@ class NajaNetlistTestXilinx(unittest.TestCase):
         lut2_ins1.get_term("I0").connect(net2)
         o.connect(net2)
 
-
         leaf_drivers_count = 0
         for leaf_driver in lut2_ins1.get_term("I0").get_equipotential().get_leaf_drivers():
             leaf_drivers_count += 1
         top_readers_count = 0
-        for top_reader in lut2_ins1.get_term("I0").get_equipotential().get_leaf_readers():
+        for top_reader in lut2_ins1.get_term("I0").get_equipotential().get_top_readers():
             top_readers_count += 1
-        get_top_drivers = 0
+        top_drivers_count = 0
         for top_driver in lut2_ins0.get_term("I0").get_equipotential().get_top_drivers():
-            get_top_drivers +=1
+            top_drivers_count +=1
 
         self.assertEqual(1, leaf_drivers_count)
         self.assertEqual(1, top_readers_count)
-        self.assertEqual(1, get_top_drivers)
+        self.assertEqual(1, top_drivers_count)
+        self.assertEqual(lut2_ins0.get_design(), top)
+
+        leaf_count = 0
+        for leaf in top.get_leaf_children():
+            leaf_count += 1
+        
+        self.assertEqual(2, leaf_count)
+
+        top.delete_instance_by_id(0)
+        lut2_ins1.delete()
+
 
 if __name__ == '__main__':
     faulthandler.enable()
