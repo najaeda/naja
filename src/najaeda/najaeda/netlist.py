14d36aa2f49a82772c6c70d4206633c0c006dcf4
Noam Cohen <noam.chn1@gmail.com>
2025-02-12 02:54:36 +0100

    snlid

--- src/najaeda/najaeda/netlist.py
+++ src/najaeda/najaeda/netlist.py
@@ -903,11 +903,13 @@ class Instance:
     def __get_snl_model(self):
         if self.is_top():
             return snl.SNLUniverse.get().getTopDesign()
+
         if (self.inst.getModel().getRevisionCount() != self.revisionCount 
-                or self.inst.getModel().getSNLID() != self.SNLID): 
+                or self.inst.getModel().getSNLID() != self.SNLID):
             self.inst = get_snl_instance_from_id_list(self.pathIDs)
             self.revisionCount = self.inst.getModel().getRevisionCount()
             self.SNLID = self.inst.getModel().getSNLID()
+        
         return self.inst.getModel()
 
     def __get_leaf_snl_object(self):
