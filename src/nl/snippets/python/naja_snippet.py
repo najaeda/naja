c8a3ea6ec1fdb490bf506db90fba2c2c188b184f
xtof <christophe.alex@gmail.com>
2025-03-19 09:31:10 +0100

    refactor

--- src/nl/snippets/python/naja_snippet.py
+++ src/nl/snippets/python/naja_snippet.py
@@ -3,35 +3,35 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import sys
-from naja import snl
+from naja import naja
 
 def main() -> int:
-  u = snl.NLUniverse.create()
-  db1 = snl.NLDB.create(u)
-  db2 = snl.NLDB.create(u)
-  lib0 = snl.NLLibrary.create(db1)
-  lib1 = snl.NLLibrary.create(db2)
-  lib2 = snl.NLLibrary.create(lib0)
+  u = naja.NLUniverse.create()
+  db1 = naja.NLDB.create(u)
+  db2 = naja.NLDB.create(u)
+  lib0 = naja.NLLibrary.create(db1)
+  lib1 = naja.NLLibrary.create(db2)
+  lib2 = naja.NLLibrary.create(lib0)
 
-  model0 = snl.SNLDesign.create(lib1)
+  model0 = naja.SNLDesign.create(lib1)
   print('Model:', model0)
-  i0 = snl.SNLScalarTerm.create(model0, snl.SNLTerm.Direction.Input, 'i0')
-  i0Net = snl.SNLScalarNet.create(model0, 'i0')
+  i0 = naja.SNLScalarTerm.create(model0, naja.SNLTerm.Direction.Input, 'i0')
+  i0Net = naja.SNLScalarNet.create(model0, 'i0')
   i0.setNet(i0Net)
   print('ScalarTerm:', i0, 'in design:', i0.getDesign(), 'connected to:', i0.getNet())
-  i1 = snl.SNLBusTerm.create(model0, snl.SNLTerm.Direction.Input, 31, 0, 'i1')
+  i1 = naja.SNLBusTerm.create(model0, naja.SNLTerm.Direction.Input, 31, 0, 'i1')
   print('BusTerm:', i1, 'in design:', i0.getDesign(), 'connected to:', i1.getNet())
 
-  model1 = snl.SNLDesign.create(lib1)
+  model1 = naja.SNLDesign.create(lib1)
   print('Model:', model1)
-  d0 = snl.SNLDesign.create(lib2)
+  d0 = naja.SNLDesign.create(lib2)
   print('Design:', d0)
 
-  d0Input0 = snl.SNLScalarTerm.create(d0, snl.SNLTerm.Direction.Input, 'Input0')
+  d0Input0 = naja.SNLScalarTerm.create(d0, naja.SNLTerm.Direction.Input, 'Input0')
   print('ScalarTerm:', d0Input0, 'in design:', d0Input0.getDesign())
 
-  i0 = snl.SNLInstance.create(d0, model0, 'i0')
-  i1 = snl.SNLInstance.create(d0, model0)
+  i0 = naja.SNLInstance.create(d0, model0, 'i0')
+  i1 = naja.SNLInstance.create(d0, model0)
   print('Model ' + str(model0) + ' bit terms: ')
   for b in model0.getBitTerms():
     print(str(b))
