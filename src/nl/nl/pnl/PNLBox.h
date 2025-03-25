0cff278950fc22955964e990feacbdf80d2ee976
xtof <christophe.alex@gmail.com>
2025-03-25 11:57:14 +0100

    testing

--- src/nl/nl/pnl/PNLBox.h
+++ src/nl/nl/pnl/PNLBox.h
@@ -4,8 +4,8 @@
 
 #pragma once
 
+#include "PNLPoint.h"
 #include <limits>
-
 
 namespace naja { namespace NL {
 
@@ -13,17 +13,19 @@ class PNLBox {
   public:
     using Unit = long int;
     PNLBox() = default;
-    PNLBox(Unit left, Unit right, Unit top, Unit bottom):
-      left_(left), right_(right), top_(top), bottom_(bottom)
+    PNLBox(Unit left, Unit bottom, Unit right, Unit top):
+      left_(left), bottom_(bottom), right_(right), top_(top)
     {}
     Unit getLeft() const { return left_; }
+    Unit getBottom() const { return bottom_; }
     Unit getRight() const { return right_; }
     Unit getTop() const { return top_; }
-    Unit getBottom() const { return bottom_; }
+    PNLPoint getUpperRight() const { return PNLPoint(right_, top_); }
+    PNLPoint getLowerLeft() const { return PNLPoint(left_, bottom_); }
     private:
       Unit left_    {std::numeric_limits<Unit>::max()};
-      Unit right_   {std::numeric_limits<Unit>::min()};
       Unit bottom_  {std::numeric_limits<Unit>::max()};
+      Unit right_   {std::numeric_limits<Unit>::min()};
       Unit top_     {std::numeric_limits<Unit>::min()};
 };
 
--- test/nl/pnl/PNLBoxTest.cpp
+++ test/nl/pnl/PNLBoxTest.cpp
@@ -8,4 +8,11 @@
 using namespace naja::NL;
 
 TEST(PNLBoxTest, test0) {
+    auto box0 = PNLBox(5, 100, 25, 200);
+    EXPECT_EQ(box0.getLeft(), 5);
+    EXPECT_EQ(box0.getBottom(), 100);
+    EXPECT_EQ(box0.getRight(), 25);
+    EXPECT_EQ(box0.getTop(), 200);
+    EXPECT_EQ(box0.getLowerLeft(), PNLPoint(5, 100));
+    EXPECT_EQ(box0.getUpperRight(), PNLPoint(25, 200));
 }
