34501d26e2fdf8c5adda261868862a542a31377c
xtof <christophe.alex@gmail.com>
2025-06-25 14:24:53 +0200

    fix licenses

--- test/najaeda/test_najaeda_custom_primitives/primitives.py
+++ test/najaeda/test_najaeda_custom_primitives/primitives.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import logging
 from najaeda import naja
 
