eb547afc0af50822fbfcbdd6b81732ccbde7c2a0
Noam Cohen <noam.chn1@gmail.com>
2025-07-13 15:34:36 +0200

    fix for included header

--- src/nl/nl/decorators/BitVecDynamic.h
+++ src/nl/nl/decorators/BitVecDynamic.h
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2024 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
-//
 // SPDX-License-Identifier: Apache-2.0
 
 // BitVecDynamic.h
@@ -25,15 +24,15 @@ class BitVecDynamic {
 public:
   // Default: zero-length; stores in the uint64_t variant
   BitVecDynamic()
-    : _nbits(0), _data(uint64_t{0})
+    : nbits_(0), data_(uint64_t{0})
   {}
 
   // Construct from a 64-bit mask; length must be ≤ 64
   explicit BitVecDynamic(uint64_t mask, uint32_t length)
-    : _nbits(length)
+    : nbits_(length)
   {
     if (length <= 64) {
-      _data = mask;
+      data_ = mask;
     } else {
       throw std::out_of_range(
         "BitVecDynamic: cannot construct from mask when length>64");
@@ -42,32 +41,32 @@ public:
 
   // Construct a zero-filled vector of given length
   explicit BitVecDynamic(uint32_t length)
-    : _nbits(length)
+    : nbits_(length)
   {
     if (length <= 64) {
-      _data = uint64_t{0};
+      data_ = uint64_t{0};
     } else {
-      _data = std::vector<bool>(length, false);
+      data_ = std::vector<bool>(length, false);
     }
   }
 
   // Extract bit at pos (old “(bits_ >> pos)&1”)
   bool operator>>(size_t pos) const {
-    if (pos >= _nbits) return false;
-    if (std::holds_alternative<uint64_t>(_data)) {
-      return (std::get<uint64_t>(_data) >> pos) & 1;
+    if (pos >= nbits_) return false;
+    if (std::holds_alternative<uint64_t>(data_)) {
+      return (std::get<uint64_t>(data_) >> pos) & 1;
     } else {
-      return std::get<std::vector<bool>>(_data)[pos];
+      return std::get<std::vector<bool>>(data_)[pos];
     }
   }
 
   // OR a 64-bit mask in (old “bits_|=(1ULL<<i)”)
   BitVecDynamic& operator|=(uint64_t mask) {
-    if (std::holds_alternative<uint64_t>(_data)) {
-      _data = std::get<uint64_t>(_data) | mask;
+    if (std::holds_alternative<uint64_t>(data_)) {
+      data_ = std::get<uint64_t>(data_) | mask;
     } else {
-      auto &vec = std::get<std::vector<bool>>(_data);
-      for (size_t i = 0; i < std::min<uint32_t>(_nbits, 64U); ++i)
+      auto &vec = std::get<std::vector<bool>>(data_);
+      for (size_t i = 0; i < std::min<uint32_t>(nbits_, 64U); ++i)
         if ((mask >> i) & 1)
           vec[i] = true;
     }
@@ -76,13 +75,13 @@ public:
 
   // AND with mask, returns a fresh BitVecDynamic
   BitVecDynamic operator&(uint64_t mask) const {
-    if (std::holds_alternative<uint64_t>(_data)) {
-      return BitVecDynamic(std::get<uint64_t>(_data) & mask, _nbits);
+    if (std::holds_alternative<uint64_t>(data_)) {
+      return BitVecDynamic(std::get<uint64_t>(data_) & mask, nbits_);
     } else {
-      BitVecDynamic out(_nbits);
-      auto const &vec = std::get<std::vector<bool>>(_data);
-      auto &ovec      = std::get<std::vector<bool>>(out._data);
-      for (uint32_t i = 0; i < std::min<uint32_t>(_nbits, 64U); ++i)
+      BitVecDynamic out(nbits_);
+      auto const &vec = std::get<std::vector<bool>>(data_);
+      auto &ovec      = std::get<std::vector<bool>>(out.data_);
+      for (uint32_t i = 0; i < std::min<uint32_t>(nbits_, 64U); ++i)
         ovec[i] = vec[i] && (((mask >> i) & 1) != 0);
       return out;
     }
@@ -90,24 +89,20 @@ public:
 
   // Equality
   bool operator==(BitVecDynamic const& o) const {
-    if (_nbits != o._nbits) return false;
-    return _data == o._data;
+    return nbits_ == o.nbits_ && data_ == o.data_;
   }
 
   // Lexicographic <, MSB→LSB
   bool operator<(BitVecDynamic const& o) const {
-    // If both fit in uint64_t, do a direct integer compare
-    if (std::holds_alternative<uint64_t>(_data) &&
-        std::holds_alternative<uint64_t>(o._data)) {
-      return std::get<uint64_t>(_data) < std::get<uint64_t>(o._data);
+    if (std::holds_alternative<uint64_t>(data_) &&
+        std::holds_alternative<uint64_t>(o.data_)) {
+      return std::get<uint64_t>(data_) < std::get<uint64_t>(o.data_);
     }
-
-    // Otherwise compare bit-by-bit, MSB first
-    uint32_t maxb = std::max(_nbits, o._nbits);
+    uint32_t maxb = std::max(nbits_, o.nbits_);
     for (uint32_t off = 0; off < maxb; ++off) {
       size_t pos = maxb - 1 - off;
-      bool b1 = (pos < _nbits) ? ((*this) >> pos) : false;
-      bool b2 = (pos < o._nbits) ? (o >> pos)        : false;
+      bool b1 = (pos < nbits_) ? ((*this) >> pos) : false;
+      bool b2 = (pos < o.nbits_) ? (o >> pos)        : false;
       if (b1 != b2) return !b1 && b2;
     }
     return false;
@@ -115,12 +110,12 @@ public:
 
   // Convert back to uint64_t (old “static_cast<uint64_t>(bits_)”)
   explicit operator uint64_t() const {
-    uint64_t m = 0;
-    uint32_t upto = std::min<uint32_t>(_nbits, 64U);
-    if (std::holds_alternative<uint64_t>(_data)) {
-      m = std::get<uint64_t>(_data);
+    uint64_t m    = 0;
+    uint32_t upto = std::min<uint32_t>(nbits_, 64U);
+    if (std::holds_alternative<uint64_t>(data_)) {
+      m = std::get<uint64_t>(data_);
     } else {
-      auto const &vec = std::get<std::vector<bool>>(_data);
+      auto const &vec = std::get<std::vector<bool>>(data_);
       for (uint32_t i = 0; i < upto; ++i)
         if (vec[i]) m |= (uint64_t{1} << i);
     }
@@ -128,11 +123,11 @@ public:
   }
 
   // Number of bits
-  uint32_t size() const { return _nbits; }
+  uint32_t size() const { return nbits_; }
 
 private:
-  uint32_t                              _nbits;
-  std::variant<uint64_t, std::vector<bool>> _data;
+  uint32_t                               nbits_;
+  std::variant<uint64_t, std::vector<bool>> data_;
 };
 
 }} // namespace naja::NL
