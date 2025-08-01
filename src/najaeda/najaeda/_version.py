28e4da7e7c0e71b9f406f158fe263fe9d1f632d2
xtof <christophe.alex@gmail.com>
2025-08-01 12:59:55 +0200

    licensing and linting

--- src/najaeda/najaeda/_version.py
+++ src/najaeda/najaeda/_version.py
@@ -1,13 +1,15 @@
-# SPDX-FileCopyrightText: 2024 The Naja authors
+# SPDX-FileCopyrightText: 2025 The Naja authors
 # <https://github.com/najaeda/naja/blob/main/AUTHORS>
 #
 # SPDX-License-Identifier: Apache-2.0
 
 from najaeda import naja
 
+
 def version():
     """Get the version of Naja."""
     return naja.getVersion()
+
 
 def git_hash():
     """Get the git hash of Naja."""
