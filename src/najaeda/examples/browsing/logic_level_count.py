12a51cd34eda178c2c4e1ff9d1319b4891aec167
Noam Cohen <noam.chn1@gmail.com>
2025-08-11 00:38:59 +0200

    ll count example (#286)
    * reuse update
    
    * ct equi
    
    * lint
    
    * cov
    
    * cov
    
    * cov
    
    * ct
    
    * unit test fix
    
    * fix ut
    
    * lint + cov
    
    * -1 to amx
    
    * lint
    
    * cov
    
    * cov
    
    * cov
    
    * snlid
    
    * snlid
    
    * lint
    
    * lint
    
    * lint
    
    * ignore W503 W504
    
    * ignore W503 W504
    
    * Update najaeda_dle.py
    
    * get truth table in py api
    
    * coverage
    
    * lint
    
    * lefdef fix
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * cmake flag
    
    * fix version
    
    * Create codeql-config.yml
    
    * exclude tp from ql
    
    * Update codeql-config.yml
    
    * Update codeql-config.yml
    
    * Update codeql-config.yml
    
    * Update codeql-analysis.yml
    
    * Update codeql-analysis.yml
    
    * Update codeql-analysis.yml
    
    * Update codeql-config.yml
    
    * Update codeql-config.yml
    
    * Update codeql-analysis.yml
    
    * Update codeql-analysis.yml
    
    * remove codeql
    
    * orient+trans
    
    * cov
    
    * boxes api
    
    * boxes api
    
    * trans + ori test on design and instances
    
    * net types
    
    * resolve conflicts
    
    * placement status
    
    * fix
    
    * fix
    
    * fix mac-os 13 build
    
    * dle najaeda api
    
    * cov
    
    * fix
    
    * cleanup
    
    * fix
    
    * fix lint
    
    * fix
    
    * cleanup
    
    * cleanup
    
    * Fixing log
    
    * fix test
    
    * browsing example
    
    * browsing example
    
    * fix max size for truth table
    
    * BitVecDynamic.h
    
    * fix for included header
    
    * cov
    
    * cov
    
    * cov
    
    * cov
    
    * cov
    
    * cov
    
    * rename file
    
    * rename file
    
    * rename
    
    * update bool tree for parsing tt with 6 < num inputs
    
    * fixes
    
    * fixes
    
    * coverage
    
    * coverage
    
    * fix for all0 and all1
    
    * truth table get string
    
    * uncomment <6 tt
    
    * equi dot example
    
    * DNL api for get equi and get path
    
    * snl visual with vector of equis
    
    * get truth table per output
    
    * update cpptrace
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * liberty constructor update for per output truth table
    
    * check for per output tt for trivial cases(const, inv, buf...)
    
    * najaeda api
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * fix
    
    * fix
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * coverage
    
    * remove useless print
    
    * fanout example
    
    * isSequential
    
    * najaeda clock related terms api
    
    * najaeda comb arcs api
    
    * najaeda linting
    
    * najaeda linting
    
    * najaeda linting
    
    * git_sequential for term in najaeda
    
    * ll count
    
    * update

---
+++ src/najaeda/examples/browsing/logic_level_count.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: 2024 The Naja authors
+# <https://github.com/najaeda/naja/blob/main/AUTHORS>
+#
+# SPDX-License-Identifier: Apache-2.0
+ 
+from os import path
+import sys
+import logging
+from collections import deque
+import time
+
+from najaeda import netlist
+import faulthandler
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    faulthandler.enable()
+    # Load design
+    netlist.load_primitives('xilinx')
+
+    benchmarks = path.join('..', '..', 'benchmarks')
+    top = netlist.load_verilog(path.join(benchmarks, 'verilog', 'vexriscv.v'))
+    
+    start = time.time()
+    
+    trace_back_terms = list(top.get_flat_output_terms())
+    for leaf in top.get_leaf_children():
+        if leaf.is_sequential():
+            for term in leaf.get_flat_input_terms():
+                if term.is_sequential():
+                    trace_back_terms.append(term)
+    
+    global_max_logic_level = 0
+    path2ll = {}
+    for termToTrace in trace_back_terms:
+        queue = deque([(termToTrace, 0)])
+        max_logic_level = 0
+        visited = set() # To avoid cycles
+        while len(queue) > 0:
+            term, logic_level = queue.popleft()
+            if term in visited:
+                continue
+            visited.add(term)
+            if logic_level > max_logic_level:
+                max_logic_level = logic_level
+            leaf_drivers = term.get_equipotential().get_leaf_drivers()
+            for driver in leaf_drivers:
+                if driver.is_sequential() or driver.get_instance().is_top():
+                    continue
+                instance = driver.get_instance()
+                key = (tuple(driver.pathIDs), tuple(driver.termIDs))
+                if key in path2ll and path2ll[key] > logic_level:
+                    continue
+                path2ll[(tuple(driver.pathIDs), tuple(driver.termIDs))] = logic_level
+                input_terms = instance.get_combinatorial_inputs(driver)
+                for input_term in input_terms:
+                    queue.append((input_term, logic_level + 1))
+        if max_logic_level > global_max_logic_level:
+            global_max_logic_level = max_logic_level
+    
+    print(global_max_logic_level)
+    end = time.time()
+    print('LL count done in', end - start, 'seconds')
+    
+    
+    
