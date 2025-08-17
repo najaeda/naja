4c20a3455167e6203d7c9c25d43ad75813e91088
xtof <christophe.alex@gmail.com>
2025-08-17 16:53:26 +0200

    refactoring and testing

---
+++ test/najaeda/test_najaeda_modeling.py
@@ -0,0 +1,61 @@
+# SPDX-FileCopyrightText: 2023 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import unittest
+from najaeda import netlist
+
+# Get the PYTHONPATH environment variable
+pythonpath = os.environ.get('PYTHONPATH')
+
+if pythonpath:
+    # os.pathsep is the separator used in PYTHONPATH (':' on Unix, ';' on Windows)
+    paths = pythonpath.split(os.pathsep)
+    print("PYTHONPATH contains the following directories:")
+    for path in paths:
+        print(path)
+else:
+    print("PYTHONPATH is not set.")
+
+liberty_benchmarks = os.environ.get('LIBERTY_BENCHMARKS_PATH')
+
+class NajaNetlistModeling(unittest.TestCase):
+    def setUp(self):
+        primitives = [os.path.join(liberty_benchmarks, "tests" , "small.lib")]
+        netlist.load_liberty(primitives)
+
+    def tearDown(self):
+        netlist.reset()
+    
+    def test(self):
+        top = netlist.create_top('top')
+        i0 = top.create_term('i0', netlist.Term.Direction.INPUT)
+        i1 = top.create_term('i1', netlist.Term.Direction.INPUT)
+        o = top.create_term('o', netlist.Term.Direction.OUTPUT)
+        ck = top.create_term('ck', netlist.Term.Direction.INPUT)
+        and2_ins = top.create_child_instance('and2', 'and2_ins')
+        ff_ins = top.create_child_instance('FF', 'ff_ins')
+        i0_net = top.create_net('i0')
+        i1_net = top.create_net('i1')
+        o_net = top.create_net('o')
+        ck_net = top.create_net('ck')
+        and2_z_net = top.create_net('and2_z')
+        i0.connect(i0_net)
+        i1.connect(i1_net)
+        o.connect(o_net)
+        ck.connect(ck_net)
+        and2_ins.get_term('I0').connect(i0_net)
+        and2_ins.get_term('I1').connect(i1_net)
+        and2_ins.get_term('Z').connect(and2_z_net)
+        ff_ins.get_term('D').connect(and2_z_net)
+        ff_ins.get_term('CK').connect(ck_net)
+        ff_ins.get_term('Q').connect(o_net)
+
+        ck_input_deps = ff_ins.get_term('CK').get_clock_related_inputs()
+        self.assertEqual(1, len(ck_input_deps))
+        self.assertEqual(ck_input_deps[0], ff_ins.get_term('D'))
+
+if __name__ == '__main__':
+    faulthandler.enable()
+    unittest.main()
---
+++ test/nl/formats/liberty/SNLLibertyConstructorTest2.cpp
@@ -0,0 +1,92 @@
+// SPDX-FileCopyrightText: 2024 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "gtest/gtest.h"
+
+#include "NLUniverse.h"
+
+#include "SNLScalarTerm.h"
+#include "SNLBusTerm.h"
+
+#include "SNLLibertyConstructor.h"
+#include "NLBitVecDynamic.h"
+#include "SNLDesignModeling.h"
+
+using namespace naja::NL;
+
+#ifndef SNL_LIBERTY_BENCHMARKS
+#define SNL_LIBERTY_BENCHMARKS "Undefined"
+#endif
+
+class SNLLibertyConstructorTest2: public ::testing::Test {
+  protected:
+    void SetUp() override {
+      NLUniverse* universe = NLUniverse::create();
+      auto db = NLDB::create(universe);
+      library_ = NLLibrary::create(db, NLLibrary::Type::Primitives, NLName("MYLIB"));
+    }
+    void TearDown() override {
+      NLUniverse::get()->destroy();
+      library_ = nullptr;
+    }
+  protected:
+    NLLibrary*  library_;
+};
+
+TEST_F(SNLLibertyConstructorTest2, test) {
+  SNLLibertyConstructor constructor(library_);
+  std::filesystem::path testPath(
+      std::filesystem::path(SNL_LIBERTY_BENCHMARKS)
+      / std::filesystem::path("benchmarks")
+      / std::filesystem::path("tests")
+      / std::filesystem::path("small.lib"));
+  constructor.construct(testPath);
+  EXPECT_EQ(NLName("small_lib"), library_->getName());
+  EXPECT_EQ(library_->getSNLDesigns().size(), 2);
+  auto and2 = library_->getSNLDesign(NLName("and2"));
+  ASSERT_NE(nullptr, and2);
+  EXPECT_EQ(3, and2->getTerms().size());
+  EXPECT_EQ(3, and2->getScalarTerms().size());
+  EXPECT_TRUE(and2->getBusTerms().empty());
+  auto i0 = and2->getScalarTerm(NLName("I0"));
+  ASSERT_NE(nullptr, i0);
+  EXPECT_EQ(SNLTerm::Direction::Input, i0->getDirection());
+  auto i1 = and2->getScalarTerm(NLName("I1"));
+  ASSERT_NE(nullptr, i1);
+  EXPECT_EQ(SNLTerm::Direction::Input, i1->getDirection());
+  auto z = and2->getScalarTerm(NLName("Z"));
+  ASSERT_NE(nullptr, z);
+  EXPECT_EQ(SNLTerm::Direction::Output, z->getDirection());
+  auto tt = SNLDesignModeling::getTruthTable(and2);
+  EXPECT_TRUE(tt.isInitialized());
+  EXPECT_EQ(2, tt.size());
+  EXPECT_EQ(NLBitVecDynamic(0b1000, 4), tt.bits());
+  //
+  //EXPECT_TRUE(SNLDesignModeling::isAnd(design));
+
+  auto ff = library_->getSNLDesign(NLName("FF"));
+  ASSERT_NE(nullptr, ff);
+  EXPECT_EQ(3, ff->getTerms().size());
+  EXPECT_EQ(3, ff->getScalarTerms().size());
+  EXPECT_TRUE(ff->getBusTerms().empty());
+  auto d = ff->getScalarTerm(NLName("D"));
+  ASSERT_NE(nullptr, d);
+  EXPECT_EQ(SNLTerm::Direction::Input, d->getDirection());
+  auto ck = ff->getScalarTerm(NLName("CK"));
+  ASSERT_NE(nullptr, ck);
+  EXPECT_EQ(SNLTerm::Direction::Input, ck->getDirection());
+  auto q = ff->getScalarTerm(NLName("Q"));
+  ASSERT_NE(nullptr, q);
+  EXPECT_EQ(SNLTerm::Direction::Output, q->getDirection());
+  auto ff_tt = SNLDesignModeling::getTruthTable(ff);
+  EXPECT_FALSE(ff_tt.isInitialized());
+  {
+    auto clocks = SNLDesignModeling::getInputRelatedClocks(d);
+    EXPECT_EQ(1, clocks.size());
+  }
+  {
+    auto clocks = SNLDesignModeling::getOutputRelatedClocks(q);
+    EXPECT_EQ(1, clocks.size());
+  }
+}
