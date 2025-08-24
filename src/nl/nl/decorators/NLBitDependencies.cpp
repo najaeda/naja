0ac8c85e5c861bca614849331d83d7a3984d08b8
Noam Cohen <noam.chn1@gmail.com>
2025-08-24 18:34:15 +0200

    cstdint in header

--- src/nl/nl/decorators/NLBitDependencies.cpp
+++ src/nl/nl/decorators/NLBitDependencies.cpp
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <cstdint>
 #include <stdexcept>
 #include <algorithm>
 #include <iostream>
--- src/nl/nl/decorators/NLBitDependencies.h
+++ src/nl/nl/decorators/NLBitDependencies.h
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include "NLID.h"
+#include <cstdint>
 
 namespace naja {
 namespace NL {
