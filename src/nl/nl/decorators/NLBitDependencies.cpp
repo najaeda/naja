6edba7ddb2ab6528d957031d413ea681bc90d36c
Noam Cohen <noam.chn1@gmail.com>
2025-08-24 17:58:34 +0200

    reuse

--- src/nl/nl/decorators/NLBitDependencies.cpp
+++ src/nl/nl/decorators/NLBitDependencies.cpp
@@ -1,5 +1,6 @@
-// SPDX-FileCopyrightText: 2024 The Naja authors <https://github.com/najaeda/naja/blob/main/AUTHORS>
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 The Naja authors
+// <https://github.com/najaeda/naja/blob/main/AUTHORS> SPDX-License-Identifier:
+// Apache-2.0
 
 #include <cstdint>
 #include <stdexcept>
