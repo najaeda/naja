eec33a8cc2eb19857b0c1c729277b04080caa6ce
xtof <christophe.alex@gmail.com>
2025-11-30 11:49:27 +0100

    new attempt

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -15,11 +15,6 @@ git clone https://github.com/microsoft/vcpkg.git $env:USERPROFILE/vcpkg
 # Install winflexbison (provides win_flex and win_bison)
 & "$env:USERPROFILE/vcpkg/vcpkg.exe" install winflexbison:x64-windows
 
-# Create symlinks so CMake FindBISON/FIND_FLEX can detect them
-$tools = "$env:USERPROFILE/vcpkg/installed/x64-windows/tools/winflexbison"
-New-Item -ItemType SymbolicLink -Path "$tools/bison.exe" -Target "$tools/win_bison.exe" -Force
-New-Item -ItemType SymbolicLink -Path "$tools/flex.exe" -Target "$tools/win_flex.exe" -Force
-
 # Export for CMake
 Add-Content $env:GITHUB_ENV "CMAKE_TOOLCHAIN_FILE=$env:USERPROFILE/vcpkg/scripts/buildsystems/vcpkg.cmake"
 Add-Content $env:GITHUB_ENV "VCPKG_ROOT=$env:USERPROFILE/vcpkg"
