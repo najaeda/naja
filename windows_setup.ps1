a054b2635839deef74299bf207a3982f482e94ad
xtof <christophe.alex@gmail.com>
2025-12-01 23:03:09 +0100

    dynamic bitset

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -11,6 +11,7 @@ git clone https://github.com/microsoft/vcpkg.git $env:USERPROFILE/vcpkg
 & "$env:USERPROFILE/vcpkg/vcpkg.exe" install capnproto:x64-windows
 & "$env:USERPROFILE/vcpkg/vcpkg.exe" install tbb:x64-windows
 & "$env:USERPROFILE/vcpkg/vcpkg.exe" install boost-intrusive:x64-windows
+& "$env:USERPROFILE/vcpkg/vcpkg.exe" install boost-dynamic-bitset:x64-windows
 
 # Export for CMake
 Add-Content $env:GITHUB_ENV "CMAKE_TOOLCHAIN_FILE=$env:USERPROFILE/vcpkg/scripts/buildsystems/vcpkg.cmake"
