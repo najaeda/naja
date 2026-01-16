b7f76da20cb7eba352aefbdcc75e2ae5266a7cc7
xtof <christophe.alex@gmail.com>
2026-01-16 18:14:09 +0100

    more testing

--- test/core/NajaLogTests.cpp
+++ test/core/NajaLogTests.cpp
@@ -6,6 +6,7 @@
 
 #include <filesystem>
 #include <fstream>
+#include <cstdlib>
 #include <string>
 
 #include "NajaLog.h"
@@ -22,6 +23,22 @@ std::string readFile(const std::filesystem::path& path) {
   std::string contents((std::istreambuf_iterator<char>(in)),
                        std::istreambuf_iterator<char>());
   return contents;
+}
+
+void setEnvVar(const char* name, const std::string& value) {
+#ifdef _WIN32
+  _putenv_s(name, value.c_str());
+#else
+  setenv(name, value.c_str(), 1);
+#endif
+}
+
+void unsetEnvVar(const char* name) {
+#ifdef _WIN32
+  _putenv_s(name, "");
+#else
+  unsetenv(name);
+#endif
 }
 }  // namespace
 
@@ -63,4 +80,82 @@ TEST(NajaLogTests, clearSinksStopsFileLogging) {
   auto contents = readFile(filePath);
   EXPECT_NE(contents.find("before clear"), std::string::npos);
   EXPECT_EQ(contents.find("after clear"), std::string::npos);
+}
+
+TEST(NajaLogTests, setLogLevelFiltersMessages) {
+  std::filesystem::path logPath(NAJA_CORE_TESTS_PATH);
+  auto filePath = logPath / "naja_log_test_log_level.log";
+
+  log::clearSinks();
+  log::addFileSink(filePath.string());
+  log::setLevel(spdlog::level::warn);
+
+  NAJA_LOG_INFO("level info suppressed");
+  NAJA_LOG_WARN("level warn visible");
+  log::get()->flush();
+
+  auto contents = readFile(filePath);
+  EXPECT_EQ(contents.find("level info suppressed"), std::string::npos);
+  EXPECT_NE(contents.find("level warn visible"), std::string::npos);
+}
+
+TEST(NajaLogTests, levelFromStringParsesLevels) {
+  EXPECT_EQ(log::levelFromString("info"), spdlog::level::info);
+  EXPECT_EQ(log::levelFromString("warn"), spdlog::level::warn);
+  EXPECT_EQ(log::levelFromString("debug"), spdlog::level::debug);
+  EXPECT_EQ(log::levelFromString("off"), spdlog::level::off);
+}
+
+TEST(NajaLogTests, shutdownResetsLogger) {
+  log::init();
+  auto logger = log::get();
+  EXPECT_NE(logger, nullptr);
+
+  log::shutdown();
+
+  auto newLogger = log::get();
+  EXPECT_NE(newLogger, nullptr);
+}
+
+TEST(NajaLogTests, setPatternFormatsOutput) {
+  std::filesystem::path logPath(NAJA_CORE_TESTS_PATH);
+  auto filePath = logPath / "naja_log_test_pattern.log";
+
+  log::clearSinks();
+  log::addFileSink(filePath.string());
+  log::setPattern("TESTPATTERN %v");
+  log::setLevel(spdlog::level::info);
+
+  NAJA_LOG_INFO("pattern check");
+  log::get()->flush();
+
+  auto contents = readFile(filePath);
+  EXPECT_NE(contents.find("TESTPATTERN"), std::string::npos);
+  EXPECT_NE(contents.find("pattern check"), std::string::npos);
+}
+
+TEST(NajaLogTests, initFromEnvUsesEnvVars) {
+  std::filesystem::path logPath(NAJA_CORE_TESTS_PATH);
+  auto filePath = logPath / "naja_log_test_env.log";
+
+  std::filesystem::remove(filePath);
+  setEnvVar("NAJA_LOG_PATTERN", "ENVTEST %v");
+  setEnvVar("NAJA_LOG_FILE", filePath.string());
+  setEnvVar("NAJA_LOG_LEVEL", "warn");
+
+  log::clearSinks();
+  log::initFromEnv();
+
+  NAJA_LOG_INFO("env info suppressed");
+  NAJA_LOG_WARN("env warn visible");
+  log::get()->flush();
+
+  auto contents = readFile(filePath);
+  EXPECT_NE(contents.find("ENVTEST"), std::string::npos);
+  EXPECT_EQ(contents.find("env info suppressed"), std::string::npos);
+  EXPECT_NE(contents.find("env warn visible"), std::string::npos);
+
+  unsetEnvVar("NAJA_LOG_PATTERN");
+  unsetEnvVar("NAJA_LOG_FILE");
+  unsetEnvVar("NAJA_LOG_LEVEL");
 }
