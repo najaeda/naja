7abdb5d364dd51f264d662906bb8c40e0c49fce8
github-action-benchmark <github@users.noreply.github.com>
2026-01-20 22:11:28 +0000

    add SNL Benchmarks (googlecpp) benchmark result for 6e5e797185deb6ce4211c13db74e4d87a04ccab3

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768947088265,
+  "repoUrl": "https://github.com/najaeda/naja",
+  "entries": {
+    "SNL Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "najaeda",
+            "username": "najaeda"
+          },
+          "committer": {
+            "name": "najaeda",
+            "username": "najaeda"
+          },
+          "id": "6e5e797185deb6ce4211c13db74e4d87a04ccab3",
+          "message": "init benchmarking",
+          "timestamp": "2026-01-20T19:15:31Z",
+          "url": "https://github.com/najaeda/naja/pull/323/commits/6e5e797185deb6ce4211c13db74e4d87a04ccab3"
+        },
+        "date": 1768947087321,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_CreateNetlist0",
+            "value": 17067.30860555649,
+            "unit": "ns/iter",
+            "extra": "iterations: 41357\ncpu: 17063.42815001088 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_CreateInstances/100",
+            "value": 85340.38559218553,
+            "unit": "ns/iter",
+            "extra": "iterations: 8190\ncpu: 85340.19365079366 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_CreateInstances/1000",
+            "value": 1136472.8689320865,
+            "unit": "ns/iter",
+            "extra": "iterations: 618\ncpu: 1135974.6779935276 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_CreateInstances/10000",
+            "value": 15850074.272727646,
+            "unit": "ns/iter",
+            "extra": "iterations: 44\ncpu: 15849478.522727273 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
