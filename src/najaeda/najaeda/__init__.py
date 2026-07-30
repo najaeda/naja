420b5ec7c4d02e5f4b3919b047cdbc0347c45d75
Christophe Alexandre <christophe.alexandre@keplertech.io>
2026-07-31 00:13:49 +0200

    fix build error due to module shadowing (#417)
    * fix build error due to module shadowing
    
    * linting fix

--- src/najaeda/najaeda/__init__.py
+++ src/najaeda/najaeda/__init__.py
@@ -5,16 +5,21 @@
 
 import importlib
 
-try:
-    naja = importlib.import_module(".naja", __name__)
-except ModuleNotFoundError as error:
-    if error.name != f"{__name__}.naja":
-        raise
-    # Some development layouts expose the extension directly on PYTHONPATH.
-    naja = importlib.import_module("naja")
 
-from ._version import version, git_hash
-from ._logging import configure_native_logging
+def _load_naja():
+    try:
+        return importlib.import_module(".naja", __name__)
+    except ModuleNotFoundError as error:
+        if error.name != f"{__name__}.naja":
+            raise
+        # Some development layouts expose the extension directly on PYTHONPATH.
+        return importlib.import_module("naja")
+
+
+naja = _load_naja()
+
+from ._version import version, git_hash  # noqa: E402
+from ._logging import configure_native_logging  # noqa: E402
 
 __version__ = version()
 
